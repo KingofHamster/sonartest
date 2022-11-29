b0bb47e026a570cb5bed72b37f38c0c6fc6b97eb
KingofHamster <90180471+KingofHamster@users.noreply.github.com>
2022-11-29 21:22:21 +0800

    Test2

--- src/main/java/org/example/MainTest.java
+++ src/main/java/org/example/MainTest.java
@@ -4,5 +4,6 @@ public class MainTest {
     public static void main(String[] args) {
         System.out.println("Test");
         System.out.println("test1");
+        System.out.println("test1");
     }
 }
