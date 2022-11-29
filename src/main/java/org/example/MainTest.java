01df4dbd01774972fc08acadceaa082b07126f76
KingofHamster <90180471+KingofHamster@users.noreply.github.com>
2022-11-29 21:18:36 +0800

    submit

--- src/main/java/org/example/MainTest.java
+++ src/main/java/org/example/MainTest.java
@@ -3,6 +3,6 @@ package org.example;
 public class MainTest {
     public static void main(String[] args) {
         System.out.println("Test");
-        System.out.println("test");
+        System.out.println("test1");
     }
 }
