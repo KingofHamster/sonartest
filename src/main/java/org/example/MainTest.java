b163fdb9b1d9487d22e58df770e12e95e506838b
KingofHamster <90180471+KingofHamster@users.noreply.github.com>
2022-11-30 10:05:53 +0800

    jenkins test

--- src/main/java/org/example/MainTest.java
+++ src/main/java/org/example/MainTest.java
@@ -3,7 +3,5 @@ package org.example;
 public class MainTest {
     public static void main(String[] args) {
         System.out.println("Test");
-        System.out.println("test1");
-        System.out.println("test1");
     }
 }
