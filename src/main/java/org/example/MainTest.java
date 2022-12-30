eb6fd745e2f4d52fd30ffdd38b9bbd30441fea91
KingofHamster <90180471+KingofHamster@users.noreply.github.com>
2022-12-30 14:52:35 +0800

    jenkins test

--- src/main/java/org/example/MainTest.java
+++ src/main/java/org/example/MainTest.java
@@ -4,6 +4,6 @@ public class MainTest {
     public static void main(String[] args) {
         String s1 = "s1";
         String s2 = "s2";
-        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s2));
     }
 }
